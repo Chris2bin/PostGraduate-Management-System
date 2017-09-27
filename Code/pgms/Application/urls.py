a48bef775d81ccd868f7bf5c58581693d338d402
koonhua <koonhua310@gmail.com>
2017-09-27 22:07:20 +0800

    Update urls.py

--- Code/pgms/Application/urls.py
+++ Code/pgms/Application/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^list/(?P<apply_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^list/(?P<apply_id>[0-9]+)/delete_apply/$', views.delete_apply, name='delete_apply'),
     url(r'^list/$', views.list, name='list'),
-    url(r'^apply/$', views.ApplyRequire, name='ApplyRequire'),
+
     
 ]
