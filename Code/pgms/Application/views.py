4c50c1bec6bc1ce6f2b860fa7505e1f8f06a78ed
koonhua <koonhua310@gmail.com>
2017-09-27 22:06:48 +0800

    Update views.py

--- Code/pgms/Application/views.py
+++ Code/pgms/Application/views.py
@@ -2,28 +2,6 @@ from .models import Apply, User
 from django.shortcuts import render, get_object_or_404
 from .forms import ApplyForm, UserForm
 
-def ApplyRequire(request):
-
-	if request.POST:
-		form = ApplyForm(request.POST,request.FILES)
-		app_name_first = request.POST['app_name_first']
-		app_name_last = request.POST['app_name_last']
-		app_birthday = request.POST['app_birthday']
-		app_gender = request.POST['app_gender']
-		app_ic = request.POST['app_ic']
-		app_nation = request.POST['app_nation']
-		app_address = request.POST['app_address']
-		app_file_upload = request.FILES['app_file_upload']
-		app_file_upload2 = request.FILES['app_file_upload2']
-		app_file_upload3 = request.FILES['app_file_upload3']
-		app_email = request.POST['app_email']
-		app_mobile_number = request.POST['app_mobile_number']
-		app_type = request.POST['app_type']
-		app_programme = request.POST['app_programme']
-		if form.is_valid():
-			form.save()
-	return render(request, 'Application/registration_form.html', {})
-
 
 def list(request):
 	if not request.user.is_authenticated():
